5faa47874644bc7d116e0a6edf5807a2978c9dd5
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-12 10:34:39 +0100

    See if I can create a commit link to GH

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,1 +1,9 @@
-export const buildCommit = () => process.env.COMMIT_REF || "local build";
+export const buildCommit = () => {
+	const commit = process.env.COMMIT_REF || "local build";
+	if (process.env.COMMIT_REF) {
+		const commitSlice = commit.slice(0, 7);
+		const commitLink = `<a href="https://github.com/apleasantview/eleventy-plugin-baseline/commit/${commit}">${commitSlice}</a>`
+		return commitLink;
+	}
+	return commit;
+};
