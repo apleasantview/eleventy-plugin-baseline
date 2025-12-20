dd5505519718fc0b7b74b61c48c9f0bcc8790ff8
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-21 00:19:28 +0100

    Shuffle some lines

--- _baseline/eleventy.config.js
+++ _baseline/eleventy.config.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
+import globals from "./globals.js";
 import debug from "./debug.js";
 import filters from "./filters.js";
 import modules from "./modules.js";
 import shortcodes from "./shortcodes.js";
-import globals from "./globals.js";
 
 /**
  * Eleventy Plugin Baseline
@@ -17,7 +17,7 @@ export default function baseline(options = {}) {
 			// Emit a warning message if the application is not using Eleventy 3.0 or newer (including prereleases).
 			eleventyConfig.versionCheck(">=3.0");
 		} catch (e) {
-			console.log(`[eleventy-baseline] WARN Eleventy plugin compatibility: ${e.message}`);
+			console.log(`[eleventy-plugin-baseline] WARN Eleventy plugin compatibility: ${e.message}`);
 		}
 
 		const userOptions = {
@@ -29,13 +29,13 @@ export default function baseline(options = {}) {
 
 		// Core functions.
 		eleventyConfig.addGlobalData("_baseline", userOptions);
+		globals(eleventyConfig);
 		eleventyConfig.addPassthroughCopy({ "./src/static": "/" }, { failOnError: true });
 		eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
 			if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
 				return false;
 			}
 		});
-		globals(eleventyConfig);
 
 		// Modules.
 		eleventyConfig.addPlugin(modules.EleventyHtmlBasePlugin, { baseHref: process.env.URL || eleventyConfig.pathPrefix });
@@ -43,6 +43,7 @@ export default function baseline(options = {}) {
 		eleventyConfig.addPlugin(modules.assetsPostCSS);
 		eleventyConfig.addPlugin(modules.assetsESBuild);
 		eleventyConfig.addPlugin(modules.headCore);
+		eleventyConfig.addPlugin(modules.sitemapCore, { enableSitemapTemplate: userOptions.enableSitemapTemplate });
 
 		// Filters — Module filters might move to their respective module.
 		eleventyConfig.addFilter("markdownify", filters.markdownFilter);
@@ -57,7 +58,6 @@ export default function baseline(options = {}) {
 		eleventyConfig.addFilter("_json", debug.json);
 		eleventyConfig.addFilter("_keys", debug.keys);
 		eleventyConfig.addPlugin(modules.navigatorCore, { enableNavigatorTemplate: userOptions.enableNavigatorTemplate });
-		eleventyConfig.addPlugin(modules.sitemapCore, { enableSitemapTemplate: userOptions.enableSitemapTemplate });
 	};
 }
 
