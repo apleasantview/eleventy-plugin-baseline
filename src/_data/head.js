bae6879fea170055f963da28668412db23cf3baa
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-23 01:22:01 +0100

    Small refactors

--- src/_data/head.js
+++ src/_data/head.js
@@ -1,8 +1,4 @@
 export default {
-	link: [
-		{ rel: "stylesheet", href: "/assets/css/index.css" }
-	],
-	script: [
-		{ src: "/assets/js/index.js", defer: true}
-	]
+	link: [ { rel: "stylesheet", href: "/assets/css/index.css" } ],
+	script: [ { src: "/assets/js/index.js", defer: true} ]
 }
