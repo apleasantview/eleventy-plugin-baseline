81f484b555eaa14a80ff32be32db98debd5e2edc
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-25 09:07:57 +0100

    Quick docs fixes

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,9 +1,10 @@
 export const buildCommit = () => {
-	const commit = process.env.COMMIT_REF || 'local build';
 	if (process.env.COMMIT_REF) {
+		const hash = process.env.COMMIT_REF;
 		const commitSlice = commit.slice(0, 7);
-		const commitLink = `<a href="https://github.com/apleasantview/eleventy-plugin-baseline/commit/${commit}">${commitSlice}</a>`;
-		return commitLink;
+		const commit = `<a href="https://github.com/apleasantview/eleventy-plugin-baseline/commit/${hash}">${commitSlice}</a>`;
+		return commit;
 	}
+	const commit = 'local build';
 	return commit;
 };
