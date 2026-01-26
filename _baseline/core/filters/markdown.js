fd1bf235eeb8193c56caf04bed796b95070997df
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-26 10:54:10 +0100

    Escape html by default in markdown filter

--- _baseline/core/filters/markdown.js
+++ _baseline/core/filters/markdown.js
@@ -1,9 +1,7 @@
 // See https://jeremias.codes/2025/02/markdown-filters-eleventy/
 import markdownit from 'markdown-it';
 
-const md = markdownit({
-	html: true
-});
+const md = markdownit();
 
 export const markdownFilter = (string) => {
 	return md.renderInline(string);
