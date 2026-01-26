0ffaf43d615c2e35754907056912874a0fedc9aa
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-26 10:43:48 +0100

    Add guard to filter

--- _baseline/core/filters/related-posts.js
+++ _baseline/core/filters/related-posts.js
@@ -1,6 +1,5 @@
 export default function relatedPostsFilter(collection = []) {
-	const page = this.ctx.page;
-	return collection.filter((post) => {
-		return post.url !== page.url;
-	});
+	const page = this?.ctx?.page;
+	if (!page?.url) return collection;
+	return collection.filter((post) => post.url !== page.url);
 }
