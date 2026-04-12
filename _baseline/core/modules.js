bf1d6e81483b5e5a08b97baa79c931fe2107104e
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-04-12 22:58:20 +0200

    Remove obsolete imports

--- _baseline/core/modules.js
+++ _baseline/core/modules.js
@@ -5,8 +5,6 @@ import { EleventyHtmlBasePlugin } from '@11ty/eleventy';
 import multilangCore from '../modules/multilang-core/plugins/multilang-core.js';
 import navigatorCore from '../modules/navigator-core/plugins/navigator-core.js';
 import assetsCore from '../modules/assets-core/plugins/assets-core.js';
-import assetsPostCSS from '../modules/assets-postcss/plugins/assets-postcss.js';
-import assetsESBuild from '../modules/assets-esbuild/plugins/assets-esbuild.js';
 import headCore from '../modules/head-core/plugins/head-core.js';
 import sitemapCore from '../modules/sitemap-core/plugins/sitemap-core.js';
 
@@ -15,8 +13,6 @@ export default {
 	multilangCore,
 	navigatorCore,
 	assetsCore,
-	assetsPostCSS,
-	assetsESBuild,
 	headCore,
 	sitemapCore
 };
