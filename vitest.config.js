61cc43ea18f75d5feadf329e2c07d7d2f93f99de
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-05-17 23:18:00 +0200

    Explicit `temp/` exclude

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		include: ['./_baseline/**/__tests__/**/*.test.js'],
+		exclude: [...configDefaults.exclude, 'temp/**'],
 		reporters: ['verbose']
 	}
 });
