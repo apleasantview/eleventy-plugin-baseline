8b6a78556c529ac1930867f66a7a9e2bd82ffa71
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-06-28 00:20:20 +0200

    Wire navigation as data file

---
+++ src/_data/navigation.js
@@ -0,0 +1,27 @@
+const header = [
+	{
+		label: 'Documentation',
+		url: '/docs/'
+	},
+	{
+		label: 'About',
+		url: '/about/'
+	}
+];
+
+const footer = [
+	{
+		label: 'Navigator',
+		url: '/navigator-core.html'
+	},
+	{
+		label: 'Internal links',
+		url: '/system/internal-links/'
+	},
+	{
+		label: 'Outgoing links',
+		url: '/system/outgoing-links/'
+	}
+];
+
+export { header, footer };
