7b0cf7274b9beccff7d0d5a75301f320d33f7537
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-07-30 14:27:42 +0200

    Rework navigation

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,27 +1,40 @@
-const header = [
-	{
-		label: 'Documentation',
-		url: '/docs/'
-	},
-	{
-		label: 'About',
-		url: '/about/'
-	}
-];
+export default {
+	en: {
+		main: [
+			{ label: 'Documentation', url: '/docs/' },
+			{ label: 'About', url: '/about/' }
+		],
 
-const footer = [
-	{
-		label: 'Navigator',
-		url: '/navigator-core.html'
+		footer: [
+			{ label: 'Navigator', url: '/navigator-core.html' },
+			{ label: 'Internal links', url: '/system/internal-links/' },
+			{ label: 'Outgoing links', url: '/system/outgoing-links/' }
+		]
 	},
-	{
-		label: 'Internal links',
-		url: '/system/internal-links/'
-	},
-	{
-		label: 'Outgoing links',
-		url: '/system/outgoing-links/'
-	}
-];
 
-export { header, footer };
+	nl: {
+		main: [
+			{ label: 'Documentatie', url: '/docs/' },
+			{ label: 'Over', url: '/nl/about/' }
+		],
+
+		footer: [
+			{ label: 'Navigator', url: '/navigator-core.html' },
+			{ label: 'Interne links', url: '/system/internal-links/' },
+			{ label: 'Uitgaande links', url: '/system/outgoing-links/' }
+		]
+	},
+
+	fr: {
+		main: [
+			{ label: 'Documentation', url: '/docs/' },
+			{ label: 'À propos', url: '/fr/about/' }
+		],
+
+		footer: [
+			{ label: 'Navigator', url: '/navigator-core.html' },
+			{ label: 'Liens internes', url: '/system/internal-links/' },
+			{ label: 'Liens sortants', url: '/system/outgoing-links/' }
+		]
+	}
+};
