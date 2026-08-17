43a503f2978f4495915d5d5f6f072ce613f3e997
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-08-17 17:12:49 +0200

    Run prettier

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -58,9 +58,7 @@ export default {
 
 	seo: {
 		preserveQueryParams: false,
-		ogImage: siteUrl
-			? { url: absolute('/og.jpg'), width: 1200, height: 630, alt: 'Eleventy Baseline' }
-			: null,
+		ogImage: siteUrl ? { url: absolute('/og.jpg'), width: 1200, height: 630, alt: 'Eleventy Baseline' } : null,
 		openGraph: { type: 'website' },
 		twitter: { card: 'summary_large_image' }
 	}
